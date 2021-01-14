4501a56c22808c52fb4a36d677a7b0533513eb41
Ryan Fanelli <illenafnayr@gmail.com>
2021-01-14 14:51:28 -0500

    MongoDB connected

--- server.js
+++ server.js
@@ -19,23 +19,23 @@ app.use(express.static('public'))
 // app.use('/controller', Controller)
 
 
-// //Database Connection
-// mongoose.connect(MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-// })
-// // DB connection test
-// mongoose.connection.on('error', err =>
-//   console.log(
-//     err.message,
-//     ' is Mongod not running?/Problem with Atlas Connection?'
-//   )
-// )
-// mongoose.connection.on('connected', () =>
-//   console.log('mongo connected: ', MONGODB_URI)
-// )
-// mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+//Database Connection
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+})
+// DB connection test
+mongoose.connection.on('error', err =>
+  console.log(
+    err.message,
+    ' is Mongod not running?/Problem with Atlas Connection?'
+  )
+)
+mongoose.connection.on('connected', () =>
+  console.log('mongo connected: ', MONGODB_URI)
+)
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
 // Listening
 app.listen(PORT, ()=>{
