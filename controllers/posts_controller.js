fcf4f1d66da54db39cbb45cb9f8ad97355952404
Ryan Fanelli <illenafnayr@gmail.com>
2021-01-14 18:56:24 -0500

    full CRUD post controller

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,8 +3,59 @@ const posts = express.Router()
 
 const Post = require("../models/post.js");
 
+
+//Read
 posts.get('/', (req, res) => {
-    res.send("hello world")
+    Post.find({}, (err, data) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.json(data)
+        }
+    })
+})
+
+//Create
+posts.post('/', (req, res) => {
+    Post.create(req.body, (err, data) => {
+        Post.find({}, (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(data)
+            }
+        })
+    })
+})
+
+//Update
+posts.put('/:id', (req, res) => {
+    Post.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true },
+        (err, data) => {
+            if (err) {
+                res.send(err)
+            } else {
+                Post.find({}, (err, data) => {
+                    res.json(data)
+                })
+            }
+        }
+    )
+})
+
+posts.delete('/:id', (req, res) => {
+    Post.findByIdAndRemove(req.params.id, (err, data)=>{
+        Post.find({}, (err, data)=>{
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(data)
+            }
+        })
+    })
 })
 
 
