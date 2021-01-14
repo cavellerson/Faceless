5ace2540a8f69e899cb1436b72a986fdd22ce755
Ryan Fanelli <illenafnayr@gmail.com>
2021-01-14 14:23:49 -0500

    server listens on PORT:3000, needs DB URI

---
+++ server.js
@@ -0,0 +1,43 @@
+//Dependencies
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+
+// Config
+const app = express()
+dotenv.config()
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+
+// Middleware
+app.use(express.json())
+app.use(express.static('public'))
+
+// Controllers
+// const Controller = require('./controllers/animals_controller.js')
+// app.use('/controller', Controller)
+
+
+// //Database Connection
+// mongoose.connect(MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+// })
+// // DB connection test
+// mongoose.connection.on('error', err =>
+//   console.log(
+//     err.message,
+//     ' is Mongod not running?/Problem with Atlas Connection?'
+//   )
+// )
+// mongoose.connection.on('connected', () =>
+//   console.log('mongo connected: ', MONGODB_URI)
+// )
+// mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+// Listening
+app.listen(PORT, ()=>{
+    console.log('listening on port: ', PORT)
+})
