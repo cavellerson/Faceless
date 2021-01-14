8e88210222a14d657177fe1c7b5b40a29048ad2c
Ryan Fanelli <illenafnayr@gmail.com>
2021-01-14 15:23:18 -0500

    post model draft

---
+++ models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema ({
+    username: {type: String},
+    title: {type: String},
+    body: {type: String},
+    imgsrc: {type: String},
+    votes: {type: Number, default: 0}
+    date: {type: Date}
+})
+
+
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = Post
