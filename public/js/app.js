b1486cc875958cd2cd6f172db83672283cf80720
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-01-15 06:59:56 -0500

    allows user to randomly get and adjective for username

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,35 @@
+// logs all adjectives in adjectives.js
+// console.log(adjectives);
+//returns a random word from the array of adjectives
+let randomAdjective = (array) => {
+    let rng = Math.floor(Math.random() * array.length);
+    let randomAdjective = array[rng];
+    return randomAdjective
+}
+
+//successfully logs a random adjective each time function is invoked
+// console.log(randomAdjective(adjectives)
+// );
+
 class App extends React.Component {
+    state= {
+        randomAdjective: ""
+    }
+
+    showAdjective = (event) => {
+        //testing if random name works on click
+        console.log(randomAdjective(adjectives));
+        this.setState({
+            randomAdjective: randomAdjective(adjectives)
+        })
+    }
+
     render = ()=>{
         return(
-            <h2>Hello World</h2>
+            <div>
+            <h2>One and Done</h2>
+            <button onClick={this.showAdjective}>random name: {this.state.randomAdjective}</button>
+            </div>
         )
     }
 }
